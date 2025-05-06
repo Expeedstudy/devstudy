e4c5f9fcbaeec1a2939f4849d3f1da1ae26617df
Expeedstudy <141025723+Expeedstudy@users.noreply.github.com>
2025-05-06 11:43:25 +0530

    Update index.js
    change 1 for converting to web app

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-"console.log('Hello from Node App!')" 
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from Node App!');
+});
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
